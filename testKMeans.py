c8cd603189623222e7588f2874011169b0e38073
y2311219-gif <y2311219@gl.cc.uec.ac.jp>
2025-12-20 23:28:00 +0900

    データが昇順になっていない場合のテストtest_kmeans_unsortedを追加

--- testKMeans.py
+++ testKMeans.py
@@ -10,6 +10,15 @@ class TestKMeans(unittest.TestCase):
         for data, k, expected in params:
             with self.subTest(data=data, k=k, expected=expected):
                 self.assertEqual(k_means(data, k), expected)
+                
+    def test_kmeans_unsorted(self):
+        params = [
+            ([2, 1, 4], 2, [[1, 2], [4]]),
+            ([10, 5, 6, 1], 3, [[1], [5, 6], [10]])
+        ]
+        for data, k, expected in params:
+            with self.subTest(data=data, k=k, expected=expected):
+                self.assertEqual(k_means(data, k), expected)
 
 if __name__ == "__main__":
     unittest.main()
