fd8bfa1a857f676469f83660593ebd73eb575eb6
y2311219-gif <y2311219@gl.cc.uec.ac.jp>
2025-12-20 23:09:49 +0900

    テストコードをkmeans.pyから分離

--- kmeans.py
+++ kmeans.py
@@ -1,5 +1,3 @@
-import unittest
-
 def k_means(data, k):
     d = (data[-1] - data[0]) / k
     centers = [data[0] + (i + 0.5) * d for i in range(k)]
@@ -8,16 +6,3 @@ def k_means(data, k):
         cluster_index = min(range(k), key=lambda i: abs(point - centers[i]))
         clusters[cluster_index].append(point)
     return clusters
-
-class TestKMeans(unittest.TestCase):
-    def test_kmeans_basic(self):
-        params = [
-            ([1, 2, 4], 2, [[1, 2], [4]]),
-            ([1, 5, 6, 10], 3, [[1], [5, 6], [10]])
-        ]
-        for data, k, expected in params:
-            with self.subTest(data=data, k=k, expected=expected):
-                self.assertEqual(k_means(data, k), expected)
-
-if __name__ == "__main__":
-    unittest.main()
---
+++ testKMeans.py
@@ -0,0 +1,15 @@
+import unittest
+from kmeans import k_means
+
+class TestKMeans(unittest.TestCase):
+    def test_kmeans_basic(self):
+        params = [
+            ([1, 2, 4], 2, [[1, 2], [4]]),
+            ([1, 5, 6, 10], 3, [[1], [5, 6], [10]])
+        ]
+        for data, k, expected in params:
+            with self.subTest(data=data, k=k, expected=expected):
+                self.assertEqual(k_means(data, k), expected)
+
+if __name__ == "__main__":
+    unittest.main()
