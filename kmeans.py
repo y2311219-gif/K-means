1302a59ec55107782fa8874f84ed5bfdafbee1b7
y2311219-gif <y2311219@gl.cc.uec.ac.jp>
2025-12-20 23:45:10 +0900

    データの最小値・最大値を得るときにmin関数、max関数を使用するように変更、及び返り値を結果を昇順にしたものに変更

--- kmeans.py
+++ kmeans.py
@@ -1,8 +1,9 @@
 def k_means(data, k):
-    d = (data[-1] - data[0]) / k
-    centers = [data[0] + (i + 0.5) * d for i in range(k)]
+    d = (max(data) - min(data)) / k
+    centers = [min(data) + (i + 0.5) * d for i in range(k)]
     clusters = [[] for _ in range(k)]
     for point in data:
         cluster_index = min(range(k), key=lambda i: abs(point - centers[i]))
         clusters[cluster_index].append(point)
-    return clusters
+    result = [sorted(cluster) for cluster in clusters]
+    return result
