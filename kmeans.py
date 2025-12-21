dcf3b87110ece71b6d9385c69bd1d870500a5112
y2311219-gif <y2311219@gl.cc.uec.ac.jp>
2025-12-21 22:40:31 +0900

    データ点からクラスターを構築するロジックを関数として分離

--- kmeans.py
+++ kmeans.py
@@ -3,10 +3,7 @@ def k_means(data, k):
     
     old_clusters = [[] for _ in range(k)]
     while True:
-        clusters = [[] for _ in range(k)]
-        for point in data:
-            cluster_index = min(range(k), key=lambda i: abs(point - centers[i]))
-            clusters[cluster_index].append(point)
+        clusters = _form_clusters(data, k, centers)
         if(clusters == old_clusters):
             break
 
@@ -22,3 +19,10 @@ def _init_centers(data, k):
     d = (len(data) - 1) / (k - 1)
     centers = [sorted_data[int(d * i)] for i in range(k)]
     return centers
+
+def _form_clusters(data, k, centers):
+    clusters = [[] for _ in range(k)]
+    for point in data:
+        cluster_index = min(range(k), key=lambda i: abs(point - centers[i]))
+        clusters[cluster_index].append(point)
+    return clusters
