8ef2e07b9e3464b7aae493cbf5f27aae1ee863a9
y2311219-gif <y2311219@gl.cc.uec.ac.jp>
2025-12-21 17:28:38 +0900

    重心の初期化ロジックを変更(今回のテストケースでは、複数サイクルは必須ではなかった)

--- kmeans.py
+++ kmeans.py
@@ -1,9 +1,12 @@
 def k_means(data, k):
-    d = (max(data) - min(data)) / k
-    centers = [min(data) + (i + 0.5) * d for i in range(k)]
+    sorted_data = sorted(data)
+    d = (len(data) - 1) / (k - 1)
+    centers = [sorted_data[int(d * i)] for i in range(k)]
+    
     clusters = [[] for _ in range(k)]
     for point in data:
         cluster_index = min(range(k), key=lambda i: abs(point - centers[i]))
         clusters[cluster_index].append(point)
+    
     result = [sorted(cluster) for cluster in clusters]
     return result
